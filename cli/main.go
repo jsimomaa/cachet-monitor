4ec7b3fe46c24e2482317cfcec071899f72784bd
jvictor <julien.victor@ritmx.com>
2017-05-18 14:37:32 +0200

    Fix: Use local source code instead of online one for compilation

--- cli/main.go
+++ cli/main.go
@@ -12,7 +12,7 @@ import (
 	"sync"
 
 	"github.com/Sirupsen/logrus"
-	cachet "github.com/castawaylabs/cachet-monitor"
+	"cachet"
 	docopt "github.com/docopt/docopt-go"
 	"github.com/mitchellh/mapstructure"
 	"gopkg.in/yaml.v2"
