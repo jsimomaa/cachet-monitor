ed12fa74ecc0181d6391369e0c0c75fdc6269386
VeekeeFr <20480977+VeekeeFr@users.noreply.github.com>
2018-02-01 23:09:38 +0100

    added log at the end of each component load

--- cli/main.go
+++ cli/main.go
@@ -135,6 +135,8 @@ func main() {
 		monitor.Init(cfg)
 
 		go monitor.ClockStart(cfg, monitor, wg)
+
+		logrus.Infof("Monitor #%d has been started", index)
 	}
 
 	signals := make(chan os.Signal, 1)
